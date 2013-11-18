ac8e34ba22c8ad723bb16159a8fc14d44fac03d3
Piotr Krzeminski <piotr@krzeminski.it>
2013-11-18 01:29:25 +0100

    The about view is now kept in portrait orientation.

--- src/eu/mcft/sumoremote/MainActivity.java
+++ src/eu/mcft/sumoremote/MainActivity.java
@@ -104,7 +104,7 @@ public class MainActivity extends Activity implements OnClickListener
         }
         
         this.doubleBackToExitPressedOnce = true;
-        Toast.makeText(this, "Please press BACK again to exit", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show();
         new Handler().postDelayed(new Runnable()
         {
             @Override
